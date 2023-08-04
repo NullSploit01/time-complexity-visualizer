0c0b1ff3f4dbe6927acb32e43ebdcb1107dcf1af
NullSploit01 <harshaldharmik099@gmail.com>
2023-08-04 16:35:06 +0530

    enable absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material';
-import './assets/styles/index.css';
-import { centerBox } from './assets/styles/components/center-box.style';
+import '~/assets/styles/index.css';
+import { centerBox } from '~/assets/styles/components/center-box.style';
 
 const App = () => {
   return <Box sx={centerBox.container}>Work In Progress</Box>;
