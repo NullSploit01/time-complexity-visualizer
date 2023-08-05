4cae63d9ac208e984cdc21eab3e3dcbeb5d46f72
NullSploit01 <harshaldharmik099@gmail.com>
2023-08-05 21:49:35 +0530

    update colors for line graph

--- src/constants/time-complexity.constant.ts
+++ src/constants/time-complexity.constant.ts
@@ -1,14 +1,14 @@
 import { ITimeComplexities } from 'src/types/time-complexity.type';
 
 export const TimeComplexityGraphColor: Record<ITimeComplexities, string> = {
-  constant: 'red',
-  logarithmic: 'green',
-  linear: 'blue',
-  linear_log: 'brown',
-  quadratic: 'black',
-  cubic: 'cyan',
-  exponential: 'violet',
-  factorial: 'indigo',
+  constant: '#ffe119',
+  logarithmic: '#3cb44b',
+  linear: '#e6194B',
+  linear_log: '#4363d8',
+  quadratic: '#f58231',
+  cubic: '#911eb4',
+  exponential: '#42d4f4',
+  factorial: '#000075',
 };
 
 export const TimeComplexities: ITimeComplexities[] = [
